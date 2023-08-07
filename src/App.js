ad876d6d15cbb9a4da1ccd192964ed4b0b5b9bdb
Nikita <oc2.doom@gmail.com>
2023-08-07 13:22:34 +0300

    create function 'Add' in cart

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
+import { useState } from 'react';
 import './App.css';
-import Button from './Components/Button/Button';
 import Card from './Components/Card/Card';
+import Cart from './Components/Cart/Cart';
 
 const {getData} = require("./db/db");
 
 const foods = getData();
 
 function App() {
+
+
+  const [cartItems, setCartItems] = useState([])
+
+
+  const onAdd = (food) => {
+    const exist = cartItems.find(x=>x.id === food.id);
+    if (exist){
+      setCartItems
+        (cartItems.map((x)=>
+          x.id === food.id ? {...exist, quantity: exist.quantity +1} : x)
+      );
+    } else {
+      setCartItems([...cartItems, {...food, quantity:1}]);
+    }
+  };
+
+
   return (
     <>
       <h1 className='heading'>Сделай заказ:</h1>
-    <div className='cards__container'>
-      {foods.map(food => {
-        return <Card food={food} key={food.id}/> 
-      })}
+      <Cart cartItems={cartItems}/>
+      <div className='cards__container'>
+        {foods.map(food => {
+          return <Card food={food} key={food.id}/> 
+        })}
     </div>
     </>
   );
